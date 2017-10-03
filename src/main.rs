b818a4476fa79bba7247e70566a77055e48de133
Venkata Giri Reddy <venkatagirireddy@gmail.com>
2017-10-02 20:02:02 -0600

    feat: just leech

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-extern crate leechrs;
-use leechrs::client::Client;
+extern crate leech;
+use leech::client::Client;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let file = match args.len() {
         2 => args[1].to_string(),
         _ => {
-            println!("leecher: usage: {} <torrent file>", args[0]);
+            println!("leech: usage: {} <torrent file>", args[0]);
             return;
         },
     };
